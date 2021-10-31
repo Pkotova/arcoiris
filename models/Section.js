35d71fe9f241ec6239de096e481c812d99197e23
Pkotova <petya.kotova@abv.bg>
2021-10-31 22:08:57 +0200

    added models and routers for section, task and comments

--- models/Section.js
+++ models/Section.js
@@ -1,7 +1,17 @@
 const mongoose = require('mongoose');
-
 const SectionSchema = new mongoose.Schema({
-
+    title:{
+        type: String,
+        required: true
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now
+    },
+    updatedDate: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 const Section = mongoose.model('Section', SectionSchema);
--- routes/section.js
+++ routes/section.js
@@ -2,15 +2,47 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 const session = require('express-session');
-const Contact = require("../models/Section");
+const Section = require("../models/Section");
 
 router.post('/create-section', (req, res) => {
+    const title  = req.body['title'];
+    const newSection = new Comment ({
+        content:content,
+        author:author,
+        taskId:taskId
+    })
+    newSection.save()
+        .then(data =>{
+            console.log('hi');
+        }).catch(error=> {
+        res.status(500).send({
+            message: error.message || "Some error occurred while creating the Comment"
+        })
+    })
+    res.redirect('/')
 })
-router.post('/read-sections', (req, res) => {
-})
+
 router.get('/delete-section/:id', function (req, res) {
+    Section.findByIdAndRemove(req.params.id).then((review)=>{
+        res.redirect('/')
+    }).catch((err) => {
+        console.log(err.message);
+    })
 })
 router.post('/update-section/:id', function (req, res) {
+    const filter = {_id: req.params.id};
+    const updatedComment = {
+        content: req.body['content']
+    }
+    Section.findByIdAndUpdate(filter,updatedComment,
+        function (err){
+            if (err){
+                res.send(err);
+                return;
+            }
+            res.redirect('/')
+        }
+    )
 })
 
 module.exports = router;
